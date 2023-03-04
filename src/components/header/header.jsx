1cfaad58550c4cab04207cfbd854f1589d5cd74b
dongree <dongwoo0307@naver.com>
2023-03-04 22:18:10 +0900

    create header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import styles from './header.module.css';
+import { BsFillSunFill } from 'react-icons/bs';
 
 const Header = () => {
-  return <div className={styles.header}>header</div>;
+  return (
+    <div className={styles.header}>
+      <div className={styles.lightModeBtn}>
+        <BsFillSunFill />
+      </div>
+      <ul className={styles.btns}>
+        <li className={styles.all}>All</li>
+        <li className={styles.active}>Active</li>
+        <li className={styles.completed}>Completed</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
